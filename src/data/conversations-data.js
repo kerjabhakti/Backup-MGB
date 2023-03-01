5b8d73e56920a87c65eefea61d612c2f4f45adb4
RoniAndarsyah <roni.andarsyah@gmail.com>
2023-03-01 08:56:20 +0700

    Upload Backup MGB

---
+++ src/data/conversations-data.js
@@ -0,0 +1,29 @@
+export const conversationsData = [
+  {
+    img: "/img/team-1.jpeg",
+    name: "PAK UNGGUL",
+    message: "Hi! I need more information...",
+  },
+  {
+    img: "/img/team-2.jpeg",
+    name: "PAK UNGGUL",
+    message: "Awesome work, can you...",
+  },
+  {
+    img: "/img/team-3.jpeg",
+    name: "PAK UNGGUL",
+    message: "About files I can...",
+  },
+  {
+    img: "/img/team-4.jpeg",
+    name: "PAK UNGGUL",
+    message: "Have a great afternoon...",
+  },
+  {
+    img: "/img/bruce-mars.jpeg",
+    name: "PAK UNGGUL",
+    message: "Hi! I need more information...",
+  },
+];
+
+export default conversationsData;
---
+++ src/pages/dashboard/index.js
@@ -0,0 +1,10 @@
+export * from "@/pages/dashboard/home";
+export * from "@/pages/dashboard/profile";
+// export * from "@/pages/dashboard/notification";
+export * from "@/pages/dashboard/tables";
+export * from "@/pages/dashboard/batuan";
+export * from "@/pages/dashboard/sumberdayageologi";
+export * from "@/pages/dashboard/fosil";
+export * from "@/pages/dashboard/FosilTabel";
+export * from "@/pages/dashboard/batuanedit"
+export * from "@/pages/dashboard/SdgTabel"
